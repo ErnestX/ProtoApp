d840e7356f175c133a3628a5a31d3e396b8ba1e0
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-05-04 15:29:48 -0700

    Initial Commit

---
+++ ProtoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProtoApp
+//
+//  Created by Jialiang Xiang on 2015-05-04.
+//  Copyright (c) 2015 ElementsLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProtoApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ProtoApp
+//
+//  Created by Jialiang Xiang on 2015-05-04.
+//  Copyright (c) 2015 ElementsLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
